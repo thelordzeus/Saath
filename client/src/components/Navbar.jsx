bd387762ee9a833a2cc59f5b6b2c44635571ae73
thelordzeus <arnav55125@gmail.com>
2024-03-31 20:49:35 +0530

    env setup

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { CustomButton } from "./index";
+import { logo, search, menu, thirdweb } from "../assets";
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
